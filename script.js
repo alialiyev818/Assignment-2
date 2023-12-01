690567d232f0b3ac9e42aa8fb2a63e0d5dbb9066
Ali <superguy@nobody.com>
2023-12-01 04:00:25 +0400

    Version 11 - Added the details page that opens when clicked on a product. This is done using my brand new js function added to script.js

--- script.js
+++ script.js
@@ -32,6 +32,55 @@ function displayProducts(products) {
                 <p>Stock: ${product.stock}</p>
             </div>
         `;
+        productElement.addEventListener('click', () => {
+            fetchProductDetails(product.id);
+        });
         container.appendChild(productElement);
     });
+}
+
+/*fetching the details*/
+
+async function fetchProductDetails(productId) {
+    try {
+        const response = await fetch(`https://dummyjson.com/products/${productId}`);
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const product = await response.json();
+        displayProductDetails(product);
+    } catch (error) {
+        console.error('Error fetching product details: ', error);
+    }
+}
+
+function displayProductDetails(product) {
+    // Clear the existing content
+    document.getElementById('products-container').innerHTML = '';
+
+    // Create a new div for product details
+    const detailsDiv = document.createElement('div');
+    detailsDiv.className = 'product-details';
+    detailsDiv.innerHTML = `
+        <h2>${product.title}</h2>
+        <img src="${product.thumbnail}" alt="${product.title}" class="main-image" />
+        <p>Description: ${product.description}</p>
+        <p>Price: $${product.price}</p>
+        <p>Discount: ${product.discountPercentage}%</p>
+        <p>Rating: ${product.rating}</p>
+        <p>Stock: ${product.stock}</p>
+        <p>Brand: ${product.brand}</p>
+        <p>Category: ${product.category}</p>
+        <div class="product-images">
+            ${product.images.map(image => `<img src="${image}" alt="${product.title} image" class="gallery-image">`).join('')}
+        </div>
+    `;
+    document.getElementById('products-container').appendChild(detailsDiv);
+
+    // Add click event listeners to each gallery image
+    const galleryImages = detailsDiv.querySelectorAll('.gallery-image');
+    const mainImage = detailsDiv.querySelector('.main-image');
+    galleryImages.forEach(img => img.addEventListener('click', () => {
+        mainImage.src = img.src;
+    }));
 }
